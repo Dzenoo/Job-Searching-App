9853303daaac31b92ddbc3958c4cc3b62f9d30b5
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-04-02 14:09:22 +0200

    Adding signup action

--- src/home/components/JobOpenings.js
+++ src/home/components/JobOpenings.js
@@ -1,7 +1,94 @@
+import {
+  Container,
+  Box,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+} from "@mui/material";
+import newsletter from "../../shared/assets/signup_action.png";
 import React from "react";
 
-const JobOpenings = () => {
-  return <div>JobOpenings</div>;
+const JobOpenings = ({ jobs }) => {
+  return (
+    <Container maxWidth="xl" sx={{ paddingTop: "60px" }}>
+      <Box
+        sx={{
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "20px",
+        }}
+      >
+        <span className="button_outlined">Job openings</span>
+        <Typography variant="h2" color="textPrimary" fontWeight="bold">
+          Browse jobs
+        </Typography>
+        <Typography variant="p" color="textSecondary">
+          More Opportunities: By browsing job opportunities on a job
+          <br />
+          searching app, you can access a wider range of job listings than
+          <br />
+          you might find through other channels.
+        </Typography>
+      </Box>
+
+      <Grid container justifyContent="center" padding={6} spacing={4}>
+        {jobs.map((job) => (
+          <Grid item key={job.id}>
+            <Card
+              sx={{
+                border: "1px solid #1482e8",
+                borderRadius: "60px",
+                padding: "20px",
+                maxWidth: "500px",
+              }}
+            >
+              <CardContent>
+                <Typography variant="h4">{job.title}</Typography>
+                <Typography variant="h6">{job.company}</Typography>
+                <Typography variant="p" color="textSecondary">
+                  {job.shortDescription}
+                </Typography>
+              </CardContent>
+              <CardActions
+                sx={{ display: "flex", justifyContent: "center", gap: "20px" }}
+              >
+                <Typography variant="p">{job.city}</Typography>
+                <Typography variant="p">{job.salary}</Typography>
+                <Typography variant="p">{job.time}</Typography>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <div className="signup_action">
+          <img src={newsletter} alt="newsletter" />
+          <div className="text">
+            <Typography variant="h4" color="white">
+              Set up personalized job alerts
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{
+                marginTop: "20px",
+                padding: "20px 60px",
+                backgroundColor: "#fff",
+                color: "#121212",
+              }}
+            >
+              Sign Up
+            </Button>
+          </div>
+        </div>
+      </Box>
+    </Container>
+  );
 };
 
 export default JobOpenings;
--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -4,13 +4,56 @@ import WhyJobSections from "../components/WhyJobSections";
 import HowItWorksSection from "../components/HowItWorksSection";
 import JobOpenings from "../components/JobOpenings";
 
+const JOBS = [
+  {
+    id: 1,
+    title: "Web Developer",
+    company: "Tech Solutions",
+    shortDescription:
+      "Seeking a skilled web developer to join our team and help build cutting-edge websites and applications.",
+    city: "San Francisco",
+    salary: "$80,000",
+    time: "Part Time",
+  },
+  {
+    id: 2,
+    title: "Marketing Manager",
+    company: "Bright Ideas",
+    shortDescription:
+      "Looking for a creative and driven marketing manager to lead our marketing team and drive sales growth.",
+    city: "New York City",
+    salary: "$90,000",
+    time: "Full Time",
+  },
+  {
+    id: 3,
+    title: "Graphic Designer",
+    company: "Artistic Vision",
+    shortDescription:
+      "Seeking a talented and experienced graphic designer to create stunning visuals and help bring our brand to life.",
+    city: "Los Angeles",
+    salary: "$60,000",
+    time: "Part Time",
+  },
+  {
+    id: 4,
+    title: "Sales Associate",
+    company: "Prime Sales",
+    shortDescription:
+      "Join our dynamic sales team and help us exceed our goals while delivering exceptional customer service.",
+    city: "Miami",
+    salary: "$40,000",
+    time: "Full Time",
+  },
+];
+
 const Home = () => {
   return (
     <>
       <HeroSection />
       <WhyJobSections />
       <HowItWorksSection />
-      <JobOpenings />
+      <JobOpenings jobs={JOBS} />
     </>
   );
 };
