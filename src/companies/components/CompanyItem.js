245b19f021ee51858d9e00b6e19c003dc6ce8171
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-04-03 18:08:37 +0200

    Adding companies

---
+++ src/companies/components/CompanyItem.js
@@ -0,0 +1,91 @@
+import { Card, CardActions, CardContent, Typography } from "@mui/material";
+import React from "react";
+import PropTypes from "prop-types";
+import { AiOutlineDollarCircle, AiOutlineStar } from "react-icons/ai";
+import { GrUserWorker } from "react-icons/gr";
+import { BsBriefcase } from "react-icons/bs";
+
+const CompanyItem = ({ id, logo, name, salary, employers, jobs, rating }) => {
+  return (
+    <Card id={id} sx={{ padding: "40px" }}>
+      <img src={logo} alt={name} />
+      <CardContent>
+        <Typography variant="h6" fontWeight="bold">
+          {name}
+        </Typography>
+        <Typography
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <b>{rating}</b>
+          <AiOutlineStar fill="gold" size={28} />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Typography
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+            backgroundColor: "rgba(205, 9, 195, 0.148)",
+            color: "purple",
+            fontWeight: "bold",
+            padding: "12px",
+            borderRadius: "30px",
+          }}
+        >
+          <BsBriefcase />
+          {jobs} jobs
+        </Typography>
+        <Typography
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+            backgroundColor: "rgba(23, 240, 81, 0.148)",
+            color: "green",
+            fontWeight: "bold",
+            padding: "12px",
+            borderRadius: "30px",
+          }}
+        >
+          <AiOutlineDollarCircle />
+          {salary}
+        </Typography>
+        <Typography
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+            backgroundColor: "rgba(25, 74, 236, 0.148)",
+            color: "royalblue",
+            fontWeight: "bold",
+            padding: "12px",
+            borderRadius: "30px",
+          }}
+        >
+          <GrUserWorker />
+          {employers} employers
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CompanyItem;
+
+CompanyItem.propTypes = [
+  { logo: PropTypes.string.isRequired },
+  { name: PropTypes.string.isRequired },
+  { salary: PropTypes.number.isRequired },
+  { rating: PropTypes.number.isRequired },
+  { employers: PropTypes.number.isRequired },
+  { jobs: PropTypes.number.isRequired },
+];
