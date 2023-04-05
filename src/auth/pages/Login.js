61797b1bd2695c6e13738c5c04fb442307d9f0a1
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-04-05 17:02:25 +0200

    Adding login

--- src/auth/pages/Login.js
+++ src/auth/pages/Login.js
@@ -9,8 +9,28 @@ import {
   Typography,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useFormHook } from "../../shared/hooks/useForm";
+import Input from "../../shared/components/Input";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/Validators";
 
 const Login = () => {
+  const [formState, inputHandler] = useFormHook(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
   return (
     <Container maxWidth="xs">
       <img src={logo} alt="logo" />
@@ -33,10 +53,24 @@ const Login = () => {
         </Typography>
         <form className="login_form">
           <FormControl>
-            <TextField placeholder="email" />
+            <Input
+              placeholder="email"
+              onInput={inputHandler}
+              validators={[VALIDATOR_EMAIL()]}
+              type="email"
+              id="email"
+              errorText="Please enter valid email"
+            />
           </FormControl>
           <FormControl>
-            <TextField placeholder="password" />
+            <Input
+              placeholder="password"
+              onInput={inputHandler}
+              validators={[VALIDATOR_MINLENGTH(6)]}
+              type="password"
+              id="email"
+              errorText="Please enter valid password"
+            />
           </FormControl>
           <Button variant="contained">Login</Button>
         </form>
