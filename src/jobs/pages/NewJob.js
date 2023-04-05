ebfacc7ed89d680d17417a821dc56b9aefeba79a
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-04-05 16:06:50 +0200

    Adding form hook

--- src/jobs/pages/NewJob.js
+++ src/jobs/pages/NewJob.js
@@ -1,6 +1,6 @@
 import {
   Button,
-  Checkbox,
+  Card,
   Container,
   FormControl,
   FormControlLabel,
@@ -11,142 +11,145 @@ import {
 } from "@mui/material";
 import { jobtypes } from "../../shared/data/data";
 import React from "react";
-import { useState } from "react";
 
 const NewJob = () => {
   return (
     <Container maxWidth="md" sx={{ padding: "60px", backgroundColor: "#fff" }}>
-      <form
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "40px",
-          backgroundColor: "#fff",
-          boxShadow: "0 2px 4px  #121212",
-          gap: "6em",
-        }}
-      >
-        {/* Job Title */}
-        <FormControl>
-          <label htmlFor="">Job Title</label>
-          <Typography color="textSecondary">
-            Job titles must describe position
-          </Typography>
-          <TextField placeholder="Job Title" />
-        </FormControl>
+      <Card>
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "40px",
+            gap: "3em",
+          }}
+        >
+          {/* Job Title */}
+          <FormControl>
+            <label htmlFor="">Job Title</label>
+            <Typography color="textSecondary">
+              Job titles must describe position
+            </Typography>
+            <TextField placeholder="Job Title" />
+          </FormControl>
 
-        {/* Job short description */}
-        <FormControl>
-          <label htmlFor="">Job description</label>
-          <Typography color="textSecondary">
-            Short description about job
-          </Typography>
-          <textarea style={{ height: "180px", resize: "none" }} />
-        </FormControl>
+          {/* Job short description */}
+          <FormControl>
+            <label htmlFor="">Job description</label>
+            <Typography color="textSecondary">
+              Short description about job
+            </Typography>
+            <textarea style={{ height: "180px", resize: "none" }} />
+          </FormControl>
 
-        {/* Job Location */}
-        <FormControl>
-          <label htmlFor="">Job Location work</label>
-          <Typography color="textSecondary">
-            Job titles must describe location work
-          </Typography>
-          <RadioGroup>
-            <FormControlLabel
-              value="Remote"
-              label="Remote"
-              control={<Radio />}
-            />
-            <FormControlLabel
-              value="Hybrid"
-              label="Hybrid"
-              control={<Radio />}
-            />
-            <FormControlLabel
-              value="On-Site"
-              label="On-Site"
-              control={<Radio />}
-            />
-          </RadioGroup>
-        </FormControl>
-
-        {/* Job type */}
-        <FormControl>
-          <label htmlFor="">Job type</label>
-          <Typography color="textSecondary">
-            You can select multiple job types
-          </Typography>
-          <RadioGroup>
-            {jobtypes.map((type) => (
+          {/* Job Location */}
+          <FormControl>
+            <label htmlFor="">Job Location work</label>
+            <Typography color="textSecondary">
+              Job titles must describe location work
+            </Typography>
+            <RadioGroup>
               <FormControlLabel
-                value={type.label}
-                label={type.label}
+                value="Remote"
+                label="Remote"
                 control={<Radio />}
               />
-            ))}
-          </RadioGroup>
-        </FormControl>
+              <FormControlLabel
+                value="Hybrid"
+                label="Hybrid"
+                control={<Radio />}
+              />
+              <FormControlLabel
+                value="On-Site"
+                label="On-Site"
+                control={<Radio />}
+              />
+            </RadioGroup>
+          </FormControl>
 
-        {/* Location */}
-        <FormControl>
-          <label htmlFor="">Location</label>
-          <Typography color="textSecondary">
-            Enter location where is your position
-          </Typography>
-          <TextField placeholder="e.g, London.." />
-        </FormControl>
+          {/* Job type */}
+          <FormControl>
+            <label htmlFor="">Job type</label>
+            <Typography color="textSecondary">
+              You can select multiple job types
+            </Typography>
+            <RadioGroup>
+              {jobtypes.map((type) => (
+                <FormControlLabel
+                  value={type.label}
+                  label={type.label}
+                  control={<Radio />}
+                />
+              ))}
+            </RadioGroup>
+          </FormControl>
 
-        {/* Salary */}
-        <FormControl>
-          <label htmlFor="">Salary</label>
-          <Typography color="textSecondary">
-            Enter estimated salary for that job
-          </Typography>
-          <TextField placeholder="e.g, 2000" />
-        </FormControl>
+          {/* Location */}
+          <FormControl>
+            <label htmlFor="">Location</label>
+            <Typography color="textSecondary">
+              Enter location where is your position
+            </Typography>
+            <TextField placeholder="e.g, London.." />
+          </FormControl>
 
-        {/* Seniority */}
-        <FormControl>
-          <label htmlFor="">Seniority</label>
-          <Typography color="textSecondary">
-            Enter estimated Seniority for that job
-          </Typography>
-          <RadioGroup>
-            <FormControlLabel
-              value="Junior"
-              label="Junior"
-              control={<Radio />}
-            />
-            <FormControlLabel
-              value="Medior"
-              label="Medior"
-              control={<Radio />}
-            />
-            <FormControlLabel
-              value="Senior"
-              label="Senior"
-              control={<Radio />}
-            />
-            <FormControlLabel value="Entry" label="Entry" control={<Radio />} />
-          </RadioGroup>
-        </FormControl>
+          {/* Salary */}
+          <FormControl>
+            <label htmlFor="">Salary</label>
+            <Typography color="textSecondary">
+              Enter estimated salary for that job
+            </Typography>
+            <TextField placeholder="e.g, 2000" />
+          </FormControl>
 
-        {/* Requirements */}
-        <FormControl>
-          <label htmlFor="">Requirements</label>
-          <Typography color="textSecondary">
-            Add multiple requirements
-          </Typography>
-          <textarea className="textarea" placeholder="e.g," />
-        </FormControl>
+          {/* Seniority */}
+          <FormControl>
+            <label htmlFor="">Seniority</label>
+            <Typography color="textSecondary">
+              Enter estimated Seniority for that job
+            </Typography>
+            <RadioGroup>
+              <FormControlLabel
+                value="Junior"
+                label="Junior"
+                control={<Radio />}
+              />
+              <FormControlLabel
+                value="Medior"
+                label="Medior"
+                control={<Radio />}
+              />
+              <FormControlLabel
+                value="Senior"
+                label="Senior"
+                control={<Radio />}
+              />
+              <FormControlLabel
+                value="Entry"
+                label="Entry"
+                control={<Radio />}
+              />
+            </RadioGroup>
+          </FormControl>
 
-        {/* Job Description */}
-        <FormControl>
-          <label htmlFor="">Job Description </label>
-          <Typography color="textSecondary">Add Description</Typography>
-          <TextField placeholder="e.g, " />
-        </FormControl>
-        <Button>Post</Button>
-      </form>
+          {/* Requirements */}
+          <FormControl>
+            <label htmlFor="">Requirements</label>
+            <Typography color="textSecondary">
+              Add multiple requirements
+            </Typography>
+            <textarea className="textarea" placeholder="e.g," />
+          </FormControl>
+
+          {/* Job Description */}
+          <FormControl>
+            <label htmlFor="">Job Description </label>
+            <Typography color="textSecondary">Add Description</Typography>
+            <textarea className="textarea" placeholder="e.g," />
+          </FormControl>
+          <Button variant="contained">Post</Button>
+        </form>
+      </Card>
     </Container>
   );
 };
---
+++ src/shared/hooks/useForm.js
@@ -0,0 +1,61 @@
+import { useCallback, useReducer } from "react";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "Input_Change": {
+      let formIsValid = true;
+      for (const inputId of state.inputs) {
+        if (!state.inputs[inputId]) {
+          continue;
+        }
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    }
+    case "Set_Data":
+      return {
+        inputs: action.inputs,
+        isValid: action.formIsValid,
+      };
+    default:
+      return state;
+  }
+};
+
+export const useFormHook = (initialInputs, initialFormValid) => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: initialInputs,
+    isValid: initialFormValid,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "Input_Change",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
+  const setFormData = useCallback((inputData, formValidity) => {
+    dispatch({
+      type: "Ser_Data",
+      inputs: inputData,
+      formIsValid: formValidity,
+    });
+  }, []);
+
+  return [formState, inputHandler, setFormData];
+};
