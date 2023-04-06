16274045a081d7d2bed51b8069967f07a8cc4b53
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-04-06 16:34:39 +0200

    Adding Seeker login

--- controllers/seeker-controllers.js
+++ controllers/seeker-controllers.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require("mongoose");
 const express = require("express");
 const Seeker = require("../models/Seeker");
+const HttpError = require("../models/HttpError");
 
 exports.signup = async (req, res, next) => {
   const { first_name, last_name, email, password } = req.body;
@@ -36,4 +37,29 @@ exports.signup = async (req, res, next) => {
   res.status(201).json({ createdSeeker: seeker });
 };
 
-exports.login = async (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await Seeker.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Could not find seeker", 404);
+    return next(error);
+  }
+
+  if (!existingUser) {
+    const error = new HttpError("Could not find seeke", 403);
+    return next(error);
+  }
+
+  if (existingUser.password !== password) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      403
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ message: "Logged In" });
+};
