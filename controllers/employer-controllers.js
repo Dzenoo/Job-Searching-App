7aecf753add2f915fb1c9a041130cc0c3c6edf78
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-04-06 17:36:01 +0200

    Employer signup

--- controllers/employer-controllers.js
+++ controllers/employer-controllers.js
@@ -1,3 +1,56 @@
-exports.signup = async (req, res, next) => {};
+const { default: mongoose } = require("mongoose");
+const express = require("express");
+const Employer = require("../models/Employer");
+const HttpError = require("../models/HttpError");
+
+exports.signup = async (req, res, next) => {
+  const {
+    name,
+    email,
+    password,
+    phone,
+    image,
+    rating,
+    salary,
+    employees,
+    jobs,
+  } = req.body;
+
+  let existingEmployer;
+  try {
+    existingEmployer = await Employer.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Could not find Employer", 500);
+    return next(error);
+  }
+
+  if (existingEmployer) {
+    const error = new HttpError("Employer already exist", 422);
+    return next(error);
+  }
+
+  const createdEmployer = new Employer({
+    name,
+    email,
+    password,
+    phone,
+    image:
+      "https://res.cloudinary.com/dzwb60tk1/image/upload/v1680454460/2-removebg-preview_vggazr.png",
+    rating,
+    salary,
+    employees,
+    jobs,
+  });
+
+  let employer;
+  try {
+    employer = await createdEmployer.save();
+  } catch (err) {
+    const error = new HttpError("Could not create employer", 500);
+    return next(error);
+  }
+
+  res.status(201).json({ createdEmployer: employer });
+};
 
 exports.login = async (req, res, next) => {};
