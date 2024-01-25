d146f0dfdecb92e89fece2fe661bfc94d805a041
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-01-25 16:13:46 +0100

    Adding errors socketio

--- new/src/server.ts
+++ new/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import http from "http";
 import "dotenv/config";
 
 import { connectToDatabase } from "./database/mongoose";
 import { initializePublicRoutes } from "./routes";
+import { Server } from "socket.io";
 
 async function establishDatabaseConnection(): Promise<void> {
   try {
@@ -13,19 +15,32 @@ async function establishDatabaseConnection(): Promise<void> {
   }
 }
 
-function initializeExpress(): void {
+function initializeSockets(io: Server) {
+  io.on("connection", (socket) => {
+    console.log("WebSocket Connection is established" + socket);
+  });
+}
+
+function initializeServer(): void {
   const app = express();
+  const server = http.createServer(app);
+  const io = new Server(server);
 
   app.use(cors());
   app.use(express.json());
 
   initializePublicRoutes(app);
 
-  app.listen(process.env.PORT || 3000);
+  initializeSockets(io);
+
+  server.listen(3000, () => {
+    console.log("Server is running on the port 3000");
+  });
 }
 
 async function initializeApp(): Promise<void> {
   await establishDatabaseConnection();
-  initializeExpress();
+  initializeServer();
 }
+
 initializeApp();
---
+++ new/src/utils/errors/index.ts
@@ -0,0 +1,2 @@
+export * from "./errors";
+export { asyncErrors } from "./asyncErrors";
