220ad552046b6acb94ff9aff7bb0f55634304dcf
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-01-28 21:52:22 +0100

    Adding changes app

--- old/models/Newsletter.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const NewsletterSchema = new Schema({
-  email: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Newsletter", NewsletterSchema);
