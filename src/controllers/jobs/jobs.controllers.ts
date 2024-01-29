bb154e88ea3ebcf7f92db6cc2b55c7a1514531ef
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-01-29 16:07:49 +0100

    Deleting job

--- src/controllers/jobs/jobs.controllers.ts
+++ src/controllers/jobs/jobs.controllers.ts
@@ -2,6 +2,8 @@ import { asyncErrors } from "../../errors";
 import { responseServerHandler } from "../../utils/response";
 import Job from "../../models/shared/jobs.schemas";
 import Employer from "../../models/employer/employers.schemas";
+import Application from "../../models/shared/applications.schemas";
+import Seeker from "../../models/seeker/seekers.schemas";
 
 export const createJob = asyncErrors(
   async (request, response): Promise<void> => {
@@ -100,8 +102,57 @@ export const editJob = asyncErrors(async (request, response) => {
   }
 });
 
-export const deleteJob = asyncErrors(async (_request, response) => {
+export const deleteJob = asyncErrors(async (request, response) => {
   try {
+    // @ts-ignore
+    const { employerId } = request.user;
+    const jobId = request.params.jobId;
+
+    const job = await Job.findById(jobId);
+
+    if (!job) {
+      return responseServerHandler({ message: "Job not found" }, 404, response);
+    }
+
+    if (employerId.toString() !== job.company.toString()) {
+      responseServerHandler(
+        { message: "Unauthorized, employer is not the owner of the job" },
+        403,
+        response
+      );
+      return;
+    }
+
+    const applications = await Application.find({ job: jobId });
+
+    await Job.findByIdAndDelete(jobId);
+
+    await Application.deleteMany({ job: jobId });
+
+    await Seeker.updateMany(
+      {
+        $or: [
+          { applications: { $in: applications.map((app) => app._id) } },
+          { savedJobs: jobId },
+        ],
+      },
+      {
+        $pullAll: {
+          applications: applications.map((app) => app._id),
+          savedJobs: [jobId],
+        },
+      }
+    );
+
+    await Employer.findByIdAndUpdate(employerId, {
+      $pull: { jobs: jobId },
+    });
+
+    responseServerHandler(
+      { message: "Job Deleted Successfully" },
+      200,
+      response
+    );
   } catch (error: any) {
     responseServerHandler({ message: error.message }, 400, response);
   }
