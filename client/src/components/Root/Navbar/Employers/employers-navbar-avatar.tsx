0390e985b2d8b9524dc692a72936297d5a550f71
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-02-27 12:31:45 +0100

    Adding employers Navbar

--- client/src/components/Root/Navbar/Employers/employers-navbar-avatar.tsx
+++ client/src/components/Root/Navbar/Employers/employers-navbar-avatar.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
-const EmployersNavbarAvatar = () => {
-  return <div>EmployersNavbarAvatar</div>;
+const EmployersNavbarAvatar: React.FC = () => {
+  return (
+    <div className="flex items-center gap-3">
+      <div>
+        <h1 className="text-initial-black">Tech</h1>
+      </div>
+      <div className="relative w-[3rem] h-[3rem] rounded-full">
+        <Image
+          src="/images/company.png"
+          alt="profile_navbar_picture"
+          className="w-auto h-auto object-cover"
+          fill
+        />
+      </div>
+    </div>
+  );
 };
 
 export { EmployersNavbarAvatar };
--- client/src/components/Root/Navbar/Employers/employers-navbar-links.tsx
+++ client/src/components/Root/Navbar/Employers/employers-navbar-links.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Link from "next/link";
+import { Building, Home, Search } from "lucide-react";
 
-const EmployersNavbarLinks = () => {
-  return <div>EmployersNavbarLinks</div>;
+type EmployersNavbarLinksProps = {
+  pathname: string;
+};
+
+const EmployersNavbarLinks: React.FC<EmployersNavbarLinksProps> = ({
+  pathname,
+}) => {
+  return (
+    <ul className="flex items-center gap-6">
+      {Array.from([
+        {
+          id: "1",
+          title: "Home",
+          href: "/",
+          icon: <Home />,
+        },
+        {
+          id: "2",
+          title: "Find Jobs",
+          href: "/jobs",
+          icon: <Search />,
+        },
+        {
+          id: "3",
+          title: "Find Seekers",
+          href: "/find-seekers",
+          icon: <Building />,
+        },
+      ]).map(({ href, title, id, icon }) => (
+        <Link
+          key={id}
+          href={href}
+          className={`flex items-center gap-3 text-[--gray-base-color] transition-colors hover:text-[--black-base-color] ${
+            pathname === href && "text-[#0066ff]"
+          }`}
+        >
+          <div>{icon}</div>
+          <div>
+            <h1>{title}</h1>
+          </div>
+        </Link>
+      ))}
+    </ul>
+  );
 };
 
 export { EmployersNavbarLinks };
