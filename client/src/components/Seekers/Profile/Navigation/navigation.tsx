bad4ec58dd6b0ba29f2bfd77caec4924ff237714
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-03-22 13:15:41 +0100

    Updating

--- client/src/components/Seekers/Profile/Navigation/navigation.tsx
+++ client/src/components/Seekers/Profile/Navigation/navigation.tsx
@@ -35,6 +35,7 @@ const SeekerProfileNavigation: React.FC = () => {
       <div className="flex items-center gap-3">
         {SeekerNavList.map((item) => (
           <button
+            className="transition cursor-pointer hover:text-gray-600"
             key={item.id}
             onClick={() =>
               item.id === "1"
