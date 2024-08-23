b07761f43d2c2155b470351fffd0dab4f2130911
Dzenis Gudzevic <frontprogramer@gmail.com>
2024-08-23 14:37:56 +0200

    Finish

--- client/src/components/Employers/Dashboard/navbar/EmployersDashboardNavbar.tsx
+++ client/src/components/Employers/Dashboard/navbar/EmployersDashboardNavbar.tsx
@@ -65,12 +65,11 @@ const EmployersDashboardNavbar: React.FC = () => {
     <header className="bg-white sticky top-0 bottom-0 left-0 p-3 border-r border-gray-300 dark:dark:bg-[#1b1b1b] block min-h-[83vh] h-full dark:border-r dark:border-[#1b1b1b]">
       <div className="flex flex-col gap-3">
         {NavbarActionsLinks.map(({ tooltip, id, icon, href }) => (
-          <TooltipProvider>
+          <TooltipProvider key={id}>
             <Tooltip>
               <TooltipTrigger asChild>
                 <Link
                   href={href}
-                  key={id}
                   className={`transition dark:hover:bg-[#0d0d0d] p-3 rounded-lg hover:bg-gray-100 ${
                     pathname === href &&
                     "bg-blue-100 dark:bg-[#0066ff] overflow-auto"
