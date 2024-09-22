fb69ba4e858573795e3614359d64120074735a5b
Dzenis Gudzevic <dzenisgudzevic@gmail.com>
2024-09-22 15:29:09 +0200

    Fixed env

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -10,7 +10,7 @@ type AxiosHeadersConfig = {
 // ===============================
 // Constants
 // ===============================
-const DEFAULT_API_URL = process.env.API_URL;
+const DEFAULT_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // ===============================
 // API Handlers
