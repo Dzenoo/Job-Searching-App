8e234eaa7f649a0290593fc1805547008ec5773c
Dzenoo <dzenisgudzevic18@gmail.com>
2024-10-13 15:42:13 +0200

    Fixing popular

--- client/src/components/seekers/jobs/PopularJobsInfo.tsx
+++ client/src/components/seekers/jobs/PopularJobsInfo.tsx
@@ -20,7 +20,7 @@ const PopularJobsInfo: React.FC<PopularsJobsInfoProps> = ({ jobs }) => {
         <h1 className="text-base-black">Popular Job Titles</h1>
       </div>
       <Card>
-        <CardContent>
+        <CardContent className="flex flex-col gap-5">
           {jobs?.length === 0 && (
             <div className="text-center">
               <p className="text-initial-gray">No jobs found</p>
